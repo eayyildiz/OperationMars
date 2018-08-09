9ab5e1da0573a13b4408f6de5c017c270fc70891
eayyildiz <enesayyildiz@live.com>
2018-08-09 13:41:54 +0300

    Model sınıfları eklendi

---
+++ OperationMars/Models/Plateau.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationMars.Models
+{
+    public class Plateau
+    {
+        public Plateau() {
+            Grid = new int[10, 10];
+        }
+        public Plateau(int x, int y) {
+            Grid = new int[x, y];
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int[,] Grid { get; set; }
+    }
+}
--- OperationMars/Models/Rover.cs
+++ OperationMars/Models/Rover.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
+using System.Windows;
 
 namespace OperationMars.Models
 {
     public class Rover
     {
+        public Rover() {
+            this.Location = new Point(0, 0);
+            this.Direction = Direction.North;
+            this.State = RoverState.Active;
+        }
+
+        public Rover(int x, int y) {
+            this.Location = new Point(x, y);
+            this.Direction = Direction.North;
+            this.State = RoverState.Active;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PlateauId { get; set; }
+        public Point Location { get; set; }
+        public Direction Direction { get; set; }
+        public RoverState State { get; set; }
+    }
+
+    public enum Direction {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public enum RoverState
+    {
+        Active,
+        OffWork,
+        Lost
     }
 }
